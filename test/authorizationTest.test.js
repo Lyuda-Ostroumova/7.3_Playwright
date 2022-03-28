6d1689126e3ebb2227035a362d9975cdcdb413f6
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2022-03-29 00:12:07 +0300

    est file updated

--- test/authorizationTest.test.js
+++ test/authorizationTest.test.js
@@ -1,6 +1,5 @@
-
 const { chromium } = require("playwright");
-const user = require("./user");
+const {email, password} = require("../user");
 
     //positive test
 (async () => {
@@ -9,9 +8,12 @@ const user = require("./user");
             slowMo: 5000,
           });
         const page = await browser.newPage();
-        await page.goto("https://netology.ru/?modal=sign_in");
-        await page.locator('[placeholder="Email"]').fill(user.email);
-        await page.locator('[placeholder="Пароль"]').fill(user.password);
+        await page.goto("https://netology.ru/?modal=sign_in", {
+          waitUntil: 'load',
+          timeout: 0
+          });
+        await page.locator('[placeholder="Email"]').fill(email);
+        await page.locator('[placeholder="Пароль"]').fill(password);
         await page.locator('button:has-text("Войти")').click();
         await expect(page).toHaveURL("https://netology.ru/profile");
         await expect(page.locator('class="components-pages-Profile-Programs--title--NCjbp"').toHaveText("Мои курсы и профессии"));
@@ -19,18 +21,23 @@ const user = require("./user");
     })();
 
     //negative test
-   (async () => {
-        const browser = await chromium.launch({
-            headless: false,
-            slowMo: 5000,
-          });
-        const page = await browser.newPage();
-        await page.goto("https://netology.ru/?modal=sign_in");
-        await page.locator('[placeholder="Email"]').fill(user.invalidEmail);
-        await page.locator('[placeholder="Пароль"]').fill(user.invalidPassword);
-        await page.locator('button:has-text("Войти")').click();
-        await expect(page).toHaveURL("https://netology.ru/profile");
-        await expect(page.locator('._-packages-ui-kit-components-v2-Input--error--1QFF1  div'))
-        .toHaveText('Вы ввели неправильно логин или пароль');
-        await browser.close();
-    })();
+    (async () => {
+      const browser = await chromium.launch({
+          headless: false,
+          slowMo: 5000,
+        });
+      const page = await browser.newPage();
+      await page.goto("https://netology.ru/?modal=sign_in", {
+        waitUntil: 'load',
+        timeout: 0
+        });
+      await page.locator('[placeholder="Email"]').fill("login@mail.ru");
+      await page.locator('[placeholder="Пароль"]').fill("qwerty1");
+      await page.locator('button:has-text("Войти")').click();
+      await expect(page).toHaveURL("https://netology.ru/profile");
+      await expect(page.locator('._-packages-ui-kit-components-v2-Input--error--1QFF1  div'))
+      .toHaveText('Вы ввели неправильно логин или пароль');
+      await browser.close();
+  })();
+
+   
